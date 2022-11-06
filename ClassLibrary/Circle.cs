928860973641eb12efb3589f0960d06b097fe39a
Amirhreza1 <112397177+Amirhreza1@users.noreply.github.com>
2022-11-06 14:46:14 +0100

    Update Circle.cs

--- ClassLibrary/Circle.cs
+++ ClassLibrary/Circle.cs
@@ -13,7 +13,7 @@ namespace ClassLibrary
             _radius = radius;
         }
 
-        public override string ToString() => $"Circle @({_center.X:0.0}) r: {_radius:0.0}";
+        public override string ToString() => $"Circle @({_center.X:0.0}, {_center.Y:0.0}) r: {_radius:0.0}";
         public override Vector3 Center => new Vector3(_center.X, _center.Y, 0f);
         public override float Area => (float)(_radius * _radius * Math.PI);
         public override float Circumference => (float)(_radius * 2 * Math.PI);
